9fab5ab539eeead4b594419c54c84f4c857068e4
KyleCSharp <kylelrigler@gmail.com>
2022-09-16 19:09:50 -0400

    first commit

---
+++ ClassesEX1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace ClassesEX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Car myCar = new Car();
+            myCar.year = 2021;
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Color = "White";
+            Console.WriteLine(myCar.year);
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Color);
+        }
+    }
+}
---
+++ ClassesEX1/car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesEX1
+{
+    public class Car
+    {
+       public string Make { get; set; }
+       public string Model { get; set; }
+
+       public int year { get; set; }
+
+        public string Color { get; set; }
+
+    }
+}
